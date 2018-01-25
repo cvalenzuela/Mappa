5a6a6d2c06e4eaf919dc80a2f838778ce897c7f0
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-01-25 12:48:49 -0500

    remove website

--- website/build/Mappa/js/stars.js
+++
@@ -1,8 +0,0 @@
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const stars = document.getElementsByClassName('slidingNav')[0].children[0].lastElementChild.children[0];
-  stars.className = "github-button";
-  stars.setAttribute('data-icon', 'octicon-star');
-  stars.setAttribute('data-show-count', 'true');
-});
